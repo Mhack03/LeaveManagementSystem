7672a1fe5ada6ca7e99694fd5430bc1f8aaadd9d
Mark Anthony Mordido <mark.mordido@gmail.com>
2024-09-04 00:23:03 +0800

    Seeding Default Data(IdentityRole, IdentityUser and IdentityUserRole)

--- LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
+++ LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagementSystem.Web.Data
@@ -10,6 +11,52 @@ namespace LeaveManagementSystem.Web.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = "db046d61-6c8f-4b1b-97f3-bbdf610444f8",
+                    Name = "Employee",
+                    NormalizedName = "EMPLOYEE",
+                },
+
+                new IdentityRole
+                {
+                    Id = "ba546c2c-f506-463d-976d-cfa6f6d6383d",
+                    Name = "Supervisor",
+                    NormalizedName = "SUPERVISOR",
+                },
+
+                new IdentityRole
+                {
+                    Id = "69593294-da2a-411d-ab51-b2a2039ea8ed",
+                    Name = "Administrator",
+                    NormalizedName = "ADMINISTRATOR",
+                }
+            );
+            var hasher = new PasswordHasher<IdentityUser>();
+            builder.Entity<IdentityUser>().HasData(
+                new IdentityUser
+                {
+                    Id = "28cb7da6-8e79-4467-a2bc-f30fad495996",
+                    Email = "admin@localhost.com",
+                    NormalizedEmail = "ADMIN@LOCALHOST.COM",
+                    NormalizedUserName = "ADMIN@LOCALHOST.COM",
+                    UserName = "admin@localhost.com",
+                    PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+                    EmailConfirmed = true
+
+                });
+
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string> 
+                { 
+                    RoleId = "69593294-da2a-411d-ab51-b2a2039ea8ed",
+                    UserId = "28cb7da6-8e79-4467-a2bc-f30fad495996"
+                } );
+        }
         public DbSet<LeaveType> LeaveTypes { get; set; }
     }
 }
---
+++ LeaveManagementSystem.Web/Data/Migrations/20240903161705_SeedingDefaultRolesandUser.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LeaveManagementSystem.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingDefaultRolesandUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "69593294-da2a-411d-ab51-b2a2039ea8ed", null, "Administrator", "ADMINISTRATOR" },
+                    { "ba546c2c-f506-463d-976d-cfa6f6d6383d", null, "Supervisor", "SUPERVISOR" },
+                    { "db046d61-6c8f-4b1b-97f3-bbdf610444f8", null, "Employee", "EMPLOYEE" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "28cb7da6-8e79-4467-a2bc-f30fad495996", 0, "8a58beef-f253-4a42-8939-b4d01ed75887", "admin@localhost.com", true, false, null, "ADMIN@LOCALHOST.COM", "ADMIN@LOCALHOST.COM", "AQAAAAIAAYagAAAAEAhaEdd88tvUHYCwF6qBUN905hKgPgfU2f6lTjMzsavBAPYpu3UrRgKIc9gSbHIK1Q==", null, false, "2e82efee-652e-4c34-af0d-e5764305dd13", false, "admin@localhost.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "69593294-da2a-411d-ab51-b2a2039ea8ed", "28cb7da6-8e79-4467-a2bc-f30fad495996" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ba546c2c-f506-463d-976d-cfa6f6d6383d");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "db046d61-6c8f-4b1b-97f3-bbdf610444f8");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "69593294-da2a-411d-ab51-b2a2039ea8ed", "28cb7da6-8e79-4467-a2bc-f30fad495996" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "69593294-da2a-411d-ab51-b2a2039ea8ed");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "28cb7da6-8e79-4467-a2bc-f30fad495996");
+        }
+    }
+}
